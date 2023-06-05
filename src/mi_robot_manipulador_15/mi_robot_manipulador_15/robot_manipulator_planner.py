80874ee19053c24d7fce961f2f056e274ea49958
n-rincon4 <n.rincon4@uniandes.edu.co>
2023-06-05 16:45:37 -0500

    Update Punto 3

--- src/mi_robot_manipulador_15/mi_robot_manipulador_15/robot_manipulator_planner.py
+++ src/mi_robot_manipulador_15/mi_robot_manipulador_15/robot_manipulator_planner.py
@@ -13,54 +13,54 @@ class RobotManipulatorPlanner(Node):
     def _init_(self):
         super()._init_('robot_manipulator_planner')
         self.publisher_vel = self.create_publisher(Vector3, '/robot_manipulator_goal', 10)
-        timer_period= 0.1
+        timer_period = 0.1
         # Definir dimensiones del robot 
-        self.l1 = 102.4 # dimensión 1 (cm)
-        self.l2 = 138.6  # dimensión 2 (cm)
-        self.l3 = 155  # dimensión 3 (cm)
+        self.dimension1 = 10.5  # dimensión 1 (cm)
+        self.dimension2 = 10.5  # dimensión 2 (cm)
+        self.dimension3 = 10.5    # dimensión 3 (cm)
         # Se pregunta al usuario que valores desea ingresar
         while True:
             try:
-                self.hola = 0.0
-                self.como = 0.0
-                self.estas = 0.0
-                self.x = float(input("Ingrese la coordenada x que desea de la posición del end-effector: "))
-                self.y = float(input("Ingrese la coordenada y que desea de la posición del end-effector: "))
-                self.z = float(input("Ingrese la coordenada z que desea de la posición del end-effector: "))
+                self.iniciox = 0.0
+                self.inicioy = 0.0
+                self.inicioz = 0.0
+                self.coordinate_x = float(input("Ingrese la coordenada x que desea de la posición del end-effector: "))
+                self.coordinate_y = float(input("Ingrese la coordenada y que desea de la posición del end-effector: "))
+                self.coordinate_z = float(input("Ingrese la coordenada z que desea de la posición del end-effector: "))
                 break
             except ValueError:
                 print("Entrada inválida. Por favor ingrese un número.")
         
-        self.cinematicaInversa()
+        self.inverseKinematics()
        
-        self.velocity = {'hola' : 0.0, 'como' : 0.0, 'estas' : 0.0}
+        self.velocity = {'iniciox': 0.0, 'inicioy': 0.0, 'inicioz': 0.0}
 
-    def cinematicaInversa(self):
+    def inverseKinematics(self):
         # Matrices de transformación homogénea
         T = np.array([
-            [1, 0, 0, self.x],
-            [0, 1, 0, self.y],
-            [0, 0, 1, self.z - self.l3],
+            [1, 0, 0, self.coordinate_x],
+            [0, 1, 0, self.coordinate_y],
+            [0, 0, 1, self.coordinate_z - self.dimension3],
             [0, 0, 0, 1]
         ])
 
         T0_1 = np.array([
-            [np.cos(self.hola), -np.sin(self.hola), 0, 0],
-            [np.sin(self.hola), np.cos(self.hola), 0, 0],
+            [np.cos(self.iniciox), -np.sin(self.iniciox), 0, 0],
+            [np.sin(self.iniciox), np.cos(self.iniciox), 0, 0],
             [0, 0, 1, 0],
             [0, 0, 0, 1]
         ])
 
         T1_2 = np.array([
-            [np.cos(self.como), -np.sin(self.como), 0, self.l1],
+            [np.cos(self.inicioy), -np.sin(self.inicioy), 0, self.dimension1],
             [0, 0, -1, 0],
-            [np.sin(self.como), np.cos(self.como), 0, 0],
+            [np.sin(self.inicioy), np.cos(self.inicioy), 0, 0],
             [0, 0, 0, 1]
         ])
 
         T2_3 = np.array([
-            [np.cos(self.estas), -np.sin(self.estas), 0, self.l2],
-            [np.sin(self.estas), np.cos(self.estas), 0, 0],
+            [np.cos(self.inicioz), -np.sin(self.inicioz), 0, self.dimension2],
+            [np.sin(self.inicioz), np.cos(self.inicioz), 0, 0],
             [0, 0, 1, 0],
             [0, 0, 0, 1]
         ])
@@ -69,7 +69,7 @@ class RobotManipulatorPlanner(Node):
 
         # Cálculo de los ángulos de la cinemática inversa
         theta1 = atan2(T0_3[1, 3], T0_3[0, 3])
-        theta2 = atan2(sqrt(T0_3[0, 3]*2 + T0_3[1, 3]*2) - self.l1, T0_3[2, 3])
+        theta2 = atan2(sqrt(T0_3[0, 3]*2 + T0_3[1, 3]*2) - self.dimension1, T0_3[2, 3])
         theta3 = atan2(T0_3[2, 2], -T0_3[2, 0])
 
         # Convertir ángulos a valores entre -pi y pi
@@ -99,8 +99,7 @@ def main():
     rclpy.init()
     node = RobotManipulatorPlanner()
     rclpy.spin(node)
-    node.destroy_node
     rclpy.shutdown()
 
-if __name__ == '_main_':
+if _name_ == '_main_':
     main()
